a94a44f085e1c59a2617d5d2ee55db94764dac02
mahboube89 <mahboube89r@gmail.com>
2024-11-21 20:18:55 +0100

    Add featured section with sliders

--- js/app.js
+++ js/app.js
@@ -23,3 +23,86 @@ const toggleNavbar = () => {
 navElements.forEach(element => {
     element.addEventListener("click", toggleNavbar);
 });
+
+
+// Slider
+
+const sliders = document.querySelectorAll("[data-slider]");
+
+const sliderInit = function (currentSlider) {
+
+    const sliderContainer = currentSlider.querySelector("[data-slider-container]");
+    const sliderPrevBtn = currentSlider.querySelector("[data-slider-prev]");
+    const sliderNextBtn = currentSlider.querySelector("[data-slider-next]");
+
+    const totalSliderVisibleItems = Number(getComputedStyle(currentSlider).getPropertyValue("--slider-item"));
+
+    const totalSliderItems = sliderContainer.childElementCount - totalSliderVisibleItems;
+
+    let currentSlidePos = 0;
+
+    const moveSliderItem = function() {
+        sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSlidePos].offsetLeft}px)`
+    }
+
+
+    // Next Slide
+
+    const slideNext = function() {
+        const slideEnd = currentSlidePos >= totalSliderItems;
+
+        if(slideEnd) {
+            currentSlidePos = 0;
+        } else {
+            currentSlidePos++;
+        }
+
+        moveSliderItem();
+    }
+
+    sliderNextBtn.addEventListener("click", slideNext);
+
+
+    // Previous Slide
+
+    const slidePrev = function() {
+    
+        if(currentSlidePos <= 0) {
+            currentSlidePos = totalSliderItems;
+        } else {
+            currentSlidePos--;
+        }
+
+        moveSliderItem();
+    }
+
+    sliderPrevBtn.addEventListener("click", slidePrev);
+
+    const dontHaveExtraItem = totalSliderItems <= 0;
+    if(dontHaveExtraItem) {
+        sliderNextBtn.setAttribute("desabled", "");
+        sliderPrevBtn.setAttribute("desabled", "");
+    }
+
+
+    // Auto Slider
+
+    let autoSlideInterval;
+
+    const startAutoSlide = () => autoSlideInterval = setInterval(slideNext, 3000);
+    startAutoSlide();
+
+    const stopAutoSlide = () => clearInterval(autoSlideInterval);
+
+    currentSlider.addEventListener("mouseover", stopAutoSlide);
+    currentSlider.addEventListener("mouseout", startAutoSlide);
+
+    // Responsive
+
+    window.addEventListener("resize", moveSliderItem);
+
+}
+
+for(let i =0 ; i < sliders.length; i++) {
+    sliderInit(sliders[i]);
+}
