db1b8b87b6394660fccf2698c2e11c1c36346f1b
mahboube89 <mahboube89r@gmail.com>
2024-11-21 18:27:59 +0100

    Add navbar, header section, burger menu, and header intro

--- js/app.js
+++ js/app.js
@@ -2,3 +2,24 @@
 * Copyright 2024 https://linkedin.com/in/mahboubeh-ranjbar-944132239
 */
 
+"use strict";
+
+// Navbar Toggle
+
+const navButton = document.querySelector(".nav__btn");
+const navMenuContainer = document.querySelector(".nav__menu-mobile");
+const overlay = document.querySelector(".overlay");
+
+const navElements = [navButton, overlay];
+
+// Toggle Navbar Function
+const toggleNavbar = () => {
+    navButton.classList.toggle("nav__btn--open");
+    navMenuContainer.classList.toggle("nav__menu-mobile--open");
+    overlay.classList.toggle("active");
+};
+
+// Adding Event Listeners to Each Element in navElements
+navElements.forEach(element => {
+    element.addEventListener("click", toggleNavbar);
+});
